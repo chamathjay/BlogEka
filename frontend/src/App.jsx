4dfc595d987beff4766af38674c881861d01d3cb
Chamath Jayashanka <160167403+chamathjay@users.noreply.github.com>
2025-03-29 12:14:22 +0530

    implement routing and update navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import Navbar from "./components/navbar.component.jsx";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
-        <Navbar />
+        <Routes>
+            <Route path="/" element={<Navbar />} >
+                <Route path="editor" element={<h1>Editor</h1>} />
+                <Route path="signin" element={<h1>Sign In</h1>} />
+                <Route path="signup" element={<h1>Sign Up</h1>} />
+            </Route>
+        </Routes>
     )
 }
 
