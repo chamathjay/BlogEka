09adb78a579fd398728f92e02a3a71e764ccd53e
Chamath Jayashanka <160167403+chamathjay@users.noreply.github.com>
2025-05-06 09:53:23 +0530

    add signin logic

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import bcrypt from 'bcrypt';
+import { nanoid } from 'nanoid';
+import jwt from 'jsonwebtoken';
 
 //schema
 import User from './Schema/User.js';
@@ -11,7 +13,7 @@ let PORT = 3000;
 const uri = process.env.DB_LOCATION;
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
-let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,20}$/; // regex for password
+let passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*?& ]{6,20}$/; // regex for password
 
 server.use(express.json());
 
@@ -19,8 +21,26 @@ mongoose.connect(uri, {
     autoIndex: true,
 });
 
+const formatDataToSend = (user) => {
+
+    const access_token = jwt.sign({ id: user._id }, process.env.SECRET_ACCESS_KEY)
+
+    return {
+        access_token,
+        profile_img: user.personal_info.profile_img,
+        username: user.personal_info.username,
+        fullname: user.personal_info.fullname,
+    }
+}
+
+const generateUsername = async (email) => {
+    let username = email.split("@")[0];
+    let usernameExists = await User.exists({ "personal_info.username": username }).then((result) => result);
+    usernameExists ? username = username + nanoid().substring(0, 5) : username;
+    return username;
+}
+
 server.post("/signup", (req, res) => {
-    // console.log(req.body.fullname);
     let { fullname, email, password } = req.body;
 
     // validate the data from frontend
@@ -39,22 +59,57 @@ server.post("/signup", (req, res) => {
         return res.status(403).json({ "error": "Password must be between 6 to 20 characters and contain at least one number, one uppercase and lowercase letter" });
     }
 
-    bcrypt.hash(password, 10, (err, hash) => {
+    bcrypt.hash(password, 10, async (err, hash) => {
 
-        let username = email.split("@")[0];
+        let username = await generateUsername(email);
         let user = new User({
             personal_info: { fullname, email, password: hash, username }
         });
 
         user.save().then((u) => {
-            return res.status(200).json({ user: u })
+            return res.status(200).json(formatDataToSend(u));
         })
             .catch(err => {
+                if (err.code === 11000) {
+                    return res.status(500).json({ "error": "Email already exists" });
+                }
                 return res.status(500).json({ "error": err.message });
             });
 
     });
 });
+
+server.post("/signin", (req, res) => {
+    let { email, password } = req.body;
+
+    User.findOne({ "personal_info.email": email })
+        .then((user) => {
+            if (!user) {
+                return res.status(403).json({ "error": "Email not found" });
+            }
+
+            bcrypt.compare(password, user.personal_info.password, (err, result) => {
+                if (err) {
+                    return res.status(403).json({ "error": "error occured while login, please try again" });
+                }
+
+                if (!result) {
+                    return res.status(403).json({ "error": "Invalid password" });
+                } else {
+                    return res.status(200).json(formatDataToSend(user));
+                }
+            }
+            )
+
+            // console.log(user);
+            // return res.json({ "status": "got user document" });
+        })
+        .catch(err => {
+            return res.status(500).json({ "error": err.message });
+        });
+});
+
+
 
 server.listen(PORT, () => {
     console.log(`Server is listening on port -> ${PORT}`);
